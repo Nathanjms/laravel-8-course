326dbf0012451a69178fcff888e81cd827b94a4c
Nathan James <nathanjms@outlook.com>
2021-02-21 16:41:35 +0000

    New View page

--- routes/web.php
+++ routes/web.php
@@ -14,24 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/string', function () {
-    return 'string';
-});
-
-Route::get('/array', function () {
-    return [1,2,3];
-});
-
-Route::get('/json', function () {
-    return response()->json([
-        'name' => 'Nathan',
-        'course' => 'Laravel Beginners to Advanced',
-    ]);
-});
-
-Route::get('/users', function () {
-    return redirect('/');
+    return view('home');
 });
