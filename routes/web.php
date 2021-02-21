19ed8a67e38cef74eadfdeba2ec5fa34db514b3b
Nathan James <nathanjms@outlook.com>
2021-02-21 16:39:34 +0000

    basic Routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/string', function () {
+    return 'string';
+});
+
+Route::get('/array', function () {
+    return [1,2,3];
+});
+
+Route::get('/json', function () {
+    return response()->json([
+        'name' => 'Nathan',
+        'course' => 'Laravel Beginners to Advanced',
+    ]);
+});
+
+Route::get('/users', function () {
+    return redirect('/');
+});
